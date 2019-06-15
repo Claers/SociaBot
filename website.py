94177dfd4353e97f63da893c7970c8f5f4bff2e2
Unknown <ferrereflorent@hotmail.fr>
2019-06-15 17:01:48 +0400

    Set full_path instead of arg

--- website.py
+++ website.py
@@ -333,7 +333,7 @@ def oauth_callback_twitch():
     """Callback url for twitch authentification
     """
     # token = twitch_func.get_twitch_token_handmade()
-    print(request.args.get('access_token'))
+    print(request.full_path)
     token = request.args.get('access_token')
     """
     twitch = OAuth2Session(
